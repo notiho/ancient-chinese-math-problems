a774d289a0b8a05067c5ec3286eac785396838ae
Florian Keßler <florian.kessler@fau.de>
2024-12-09 12:35:41 +0100

    added example code

---
+++ check_example_code.py
@@ -0,0 +1,21 @@
+import glob
+from pathlib import Path
+
+from utils import load_all_problems, check_script_against_answer
+
+filenames = sorted(glob.glob("example_code/*.py"))
+
+problems = load_all_problems()
+problems_by_id = { p["id"]: p for p in problems }
+
+for filename in filenames:
+	with open(filename) as infile:
+		script_source = infile.read()
+	
+	problem_id = Path(filename).stem
+	
+	passed_check, message = check_script_against_answer(script_source, problems_by_id[problem_id]["answer_structured"])
+	
+	if not passed_check:
+		print(f"Script for {problem_id} failed:")
+		print(message)
